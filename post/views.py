b5e34849ec64bbdce588eca0dfbc9738ad8afc8c
oliberg15 <78539434+oliberg15@users.noreply.github.com>
2023-03-23 19:26:06 +0800

    03/23/23

--- post/views.py
+++ post/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Project
+from email_sender.forms import ContactForm
 
 
 # Create your views here.
 
 def home_page_view(request):
-    return render(request, 'home/home.html')
+    form = ContactForm()
+    return render(request, 'home/home.html',{
+        'form' : form,
+    })
 
 def profile_page_view(request):
     return render(request, 'profile/profile_detail.html')
@@ -15,9 +19,9 @@ class ResProjectPageView(ListView):
     template_name = 'project/res-project.html'
     model = Project
 
-
 class ComProjectPageView(ListView):
     template_name = 'project/com-project.html'
     model = Project
+
 
 
